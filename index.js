140678b4e819d51d4219f9a2a7a028db9145cd63
k.martens <k.martens@enk.nl>
2020-10-05 08:28:19 +0200

    added persistancy with mongoose

--- index.js
+++ index.js
@@ -3,18 +3,27 @@
 const genresRoute = require('./routes/genres');
 const homepage = require('./routes/home');
 const authenticator = require('./middleware/authenticator');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
+//setup express middleware
 app.use(express.json());
-
-
 app.set('view engine', 'pug');
 app.set('views','./views');
-
 //app.use(authenticator);
 
+//Setup routes
 app.use('/api/genres', genresRoute);
 app.use('/', homepage);
-const port = process.env.PORT || 3000;
+
+//connect to db
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to mongodb...'))
+  .catch( err => console.error('could not connect to mongodb', error));
+  
+//start webservice
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
--- routes/genres.js
+++ routes/genres.js
@@ -1,55 +1,71 @@
 
 const Joi = require('joi');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const genres = [
-    {id: 1, name: 'Action movies'},
-    {id: 2, name: 'Family movies'},
-    {id: 3, name: 'Horror'},
-    {id: 4, name: 'Scifi'}
-];
+
+const genreSchema = mongoose.Schema({
+    name: { type: String,
+            minlength: 3,
+            required: true
+        }
+});
+const Genre = mongoose.model('genre', genreSchema);
 
 
 router.get('/', (req, res) => {
-    res.send(genres);
+    Genre.find({}, (err, genres) => {
+        if (err) return res.status(500).send('Could not retrieve genres: '+err.message);
+        res.send(genres);
+    })
 });
 
 router.get('/:id', (req, res) => {
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('genre not found');
-
-    res.send(genre);
+    const id = req.params.id;
+    Genres.findById(id, (err, genre) => {
+        if (err) return res.status(500).send('could not retrieve genre: '+err.message);
+        if (!genre) return res.status(404).send(`genre with id ${id} not found`);
+        res.send(genre);
+    });
 });
+
 
 router.post('/', (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    
-    const genre = {id: genres.length +1, name: req.body.name};
-    genres.push(genre);
-    res.send(genre);
-})
+    const genre = new Genre( req.body );
+    genre.save( (err, savedGenre) => {
+        if (err) return res.status(400).send('Could not save genre: '+err.message);
+        res.send(savedGenre);
+    })
+});
 
 router.put('/:id', (req, res) => {
 
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('genre not found');
-
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    genre.name = req.body.name;
-    res.send(genre);
+    const id = req.params.id;
+    Genre.findById(id, (err, genre) => {
+        if (err)    return res.status(500).send('could not retrieve genre: '+err.message);
+        if (!genre) return res.status(404).send(`genre with id ${id} not found`);
+             
+        const { error } = validateGenre(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        genre.name = req.body.name;
+        genre.save( (err) => {
+            if (err) return res.status(500).send('Could not update genre: '+err.message);
+            res.send(genre);
+        });
+    });
 });
 
 router.delete('/:id', (req, res) => {
     //find the give id
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('genre not found');
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-    res.send(genre);
+    const id = req.params.id;
+    Genre.findByIdAndRemove(id, (err, genre) => {
+        if (err) return res.status(500).send('could not delete genre: ', err.message);
+        if (!genre) return res.status(404).send(`genre with id ${id} not found`);
+        res.send(genre);
+    });
 });
 
 function validateGenre(genre) {
@@ -59,5 +75,7 @@ function validateGenre(genre) {
     const result = schema.validate(genre);
     return result;
 };
+
+
 
 module.exports = router;
