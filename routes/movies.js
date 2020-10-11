81cf9eaaec920fcfa29ef55e837636babe574102
k.martens <k.martens@enk.nl>
2020-10-11 09:30:49 +0200

    new movies route logic

--- routes/movies.js
+++ routes/movies.js
@@ -19,7 +19,39 @@ router.put('/:id', async (req, res) => {
     const { error } = validateMovie(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const movie = await Movie.findByIdAndUpdate()
+    const id = req.body.id;
+    const movie = await Movie.findByIdAndUpdate( 
+        id, 
+        { title: req.body.title,
+          genre: req.body.genre,
+          numberInStock: req.body.numberInStock,
+          dailyRentalRate: req.body.dailyRentalRate 
+        }, 
+        { new: true}
+    );
+                
+    if (!movie) return res.status(404).send(`Movie with id ${id} not found`);
+    res.send(movie);
+    
+});
+
+router.post('/', async (req, res) => {
+    const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const movie = new Movie({
+        title : req.body.title,
+        genre : req.body.genre,
+        numberInStock : req.body.numberInStock,
+        dailyRentalRate : req.body.dailyRentalRate 
+    });
+    res.send(movie);
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    const movie = Movie.findByIdAndRemove( id );
+    if (!movie) return res.status(404).send(`Movie with id ${id} not found`);
+    res.send(movie);
 })
 
 module.exports = router;
